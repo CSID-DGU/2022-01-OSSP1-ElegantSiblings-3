10eedea7b01772a4ea00c980d4dc0d2d5ea5bc54
Dice <102275981+Dice15@users.noreply.github.com>
2022-05-25 12:18:22 +0900

    test server Build

---
+++ TestServer/TestServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+// See https://aka.ms/new-console-template for more information
+
+namespace TestServer
+{
+    class Program
+    {
+        private static bool isRunning = false;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Test Game Server";
+            isRunning = true;
+
+            Thread mainThread = new Thread(new ThreadStart(MainHread));
+            mainThread.Start();
+
+            Server.Start(50, 26950);
+        }
+
+
+        private static void MainHread()
+        {
+            Console.WriteLine($"Main thread started. Running at {Constants.TICKS_PER_SEC} ticks per second.");
+            DateTime _nextLoop = DateTime.Now;
+
+            while (isRunning)
+            {
+                while (_nextLoop < DateTime.Now)
+                {
+                    GameLogic.Update();
+                    _nextLoop = _nextLoop.AddMilliseconds(Constants.MS_PER_TICK);
+
+                    if (_nextLoop > DateTime.Now)
+                    {
+                        Thread.Sleep(_nextLoop - DateTime.Now);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ TestServer/TestServer/UserAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace TestServer
+{
+    [System.Serializable]
+    public class UserAccount
+    {
+        public string ID { get; set; } = "None";
+        public string PW { get; set; } = "None";
+    }
+
+    public static class UserAccountExtensions
+    {
+        public static UserAccount GetValueOrDefault(this UserAccount? _userAccount) => _userAccount == null ? new UserAccount() : _userAccount;     
+    }
+}
