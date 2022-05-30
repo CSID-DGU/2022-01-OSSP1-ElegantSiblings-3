a5dfac859d0951b5445d609396aeddb12279385a
JunOhS <101076609+JunOhS@users.noreply.github.com>
2022-05-30 11:31:19 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//모듈
+const express=require("express");
+const app=express();
+
+//라우팅
+const home=require("./src/routes/home");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}\src\public`));
+
+app.use("/", home); //use는 미들 웨어를 등록해주는 메서드
+
+module.exports=app;
